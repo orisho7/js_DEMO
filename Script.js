c8fb08c63e9b8e0ea59f84a3119025392471ad60
Abdullrazaq naqsho <apoodi.805@gmail.com>
2025-07-20 18:22:30 +0300

    Remove Getusernames function; consolidate user button generation into getusersaxios for improved structure

--- Script.js
+++ Script.js
@@ -1,25 +1,4 @@
-function Getusernames() {
-  return new Promise((resolve, reject) => {
-    fetch(`https://jsonplaceholder.typicode.com/users`)
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          reject("error");
-        }
-      })
-      .then((json) => {
-        for (const user of json) {
-          let btn = document.createElement("button");
-          btn.textContent = `${user.name}`;
-          btn.addEventListener("click", () => {
-            Getposts(user.id);
-          });
-          document.getElementById("user-buttons").appendChild(btn);
-        }
-      });
-  });
-}
+
 function Getposts(userId) {
   fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
     .then((response) => response.json())
