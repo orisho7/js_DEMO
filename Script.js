55cd8e9b0d9a577eb204beb9c58d37655189730f
Abdullrazaq naqsho <apoodi.805@gmail.com>
2025-07-20 18:21:11 +0300

    Refactor Getusernames function for improved readability and consistency; add axios script reference in jsRequest.html

--- Script.js
+++ Script.js
@@ -1,25 +1,24 @@
 function Getusernames() {
- return new Promise((resolve, reject) => {
-      fetch(`https://jsonplaceholder.typicode.com/users`)
-    .then((response) => {
-      if (response.ok){
-     return response.json()} else {
-        reject("error")
-     }
-    })   .then((json) => {
-      for (const user of json) {
-        let btn = document.createElement("button");
-        btn.textContent = `${user.name}`;
-        btn.addEventListener("click", () => {
-          Getposts(user.id);
-        });
-        document.getElementById("user-buttons").appendChild(btn);
-      }
-    });
- 
-  })
-
- 
+  return new Promise((resolve, reject) => {
+    fetch(`https://jsonplaceholder.typicode.com/users`)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          reject("error");
+        }
+      })
+      .then((json) => {
+        for (const user of json) {
+          let btn = document.createElement("button");
+          btn.textContent = `${user.name}`;
+          btn.addEventListener("click", () => {
+            Getposts(user.id);
+          });
+          document.getElementById("user-buttons").appendChild(btn);
+        }
+      });
+  });
 }
 function Getposts(userId) {
   fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
@@ -32,4 +31,19 @@ function Getposts(userId) {
       document.getElementById("titles").textContent = output;
     });
 }
-Getusernames()
+function getusersaxios() {
+  axios.get("https://jsonplaceholder.typicode.com/users").then((response) => {
+    let json = response.data;
+
+    for (const user of json) {
+      let btn = document.createElement("button");
+      btn.textContent = `${user.name}`;
+      btn.addEventListener("click", () => {
+        Getposts(user.id);
+      });
+      document.getElementById("user-buttons").appendChild(btn);
+    }
+  });
+}
+
+getusersaxios();
