50eb99c302c69c7b672e98a0f6d8e5f809a0f9d6
Abdullrazaq naqsho <apoodi.805@gmail.com>
2025-07-24 00:31:28 +0300

    Refactor prayer timings display and add dark/light mode toggle functionality

--- prayers/Pscript.js
+++ prayers/Pscript.js
@@ -1,48 +1,42 @@
-swiper = new Swiper(".swiper-container");
 var swiper = new Swiper(".mySwiper", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
 });
-axios
-  .get(
-    "https://api.aladhan.com/v1/timingsByCity/21-07-2025?city=Homs&country=Syria"
-  )
-  .then((response) => {
-    let Fajr = response.data.data.timings.Fajr;
-    console.log(Fajr);
-    let output = Fajr;
-    document.getElementById("test").textContent = output;
 
-    // You can display timings in your UI here
+function fillPrayers(wich) {
+  document.getElementById("Fajr").textContent = wich.Fajr;
+  document.getElementById("Duhur").textContent = wich.Dhuhr;
+  document.getElementById("Asr").textContent = wich.Asr;
+  document.getElementById("Maghrib").textContent = wich.Maghrib ;
+  document.getElementById("Isha").textContent = wich.Isha;
+}
+
+// Initial load for Syria
+axios
+  .get("https://api.aladhan.com/v1/timingsByCity/21-07-2025?city=Homs&country=Syria")
+  .then((response) => {
+    fillPrayers(response.data.data.timings);
   });
+
 swiper.on("slideChange", function () {
   const activeSlide = swiper.slides[swiper.activeIndex];
   const country = activeSlide.getAttribute("data-country");
   if (country === "Syria") {
     axios
-      .get(
-        "https://api.aladhan.com/v1/timingsByCity/21-07-2025?city=Homs&country=Syria"
-      )
+      .get("https://api.aladhan.com/v1/timingsByCity/21-07-2025?city=Homs&country=Syria")
       .then((response) => {
-        let Fajr = response.data.data.timings.Fajr;
-        console.log(Fajr);
-        let output = Fajr;
-        document.getElementById("test").textContent = output;
+        fillPrayers(response.data.data.timings);
       });
   } else if (country === "SA") {
     axios
-      .get(
-        "https://api.aladhan.com/v1/timingsByCity/21-07-2025?city=Riyadh&country=Saudi Arabia"
-      )
+      .get("https://api.aladhan.com/v1/timingsByCity/21-07-2025?city=Riyadh&country=Saudi Arabia")
       .then((response) => {
-        let Fajr = response.data.data.timings.Fajr;
-        console.log(Fajr);
-        let output = Fajr;
-        document.getElementById("test").textContent = output;
+        fillPrayers(response.data.data.timings);
       });
   } else {
+    fillPrayers({});
     console.log("not found");
   }
 });
