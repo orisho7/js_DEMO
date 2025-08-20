06ba2f02dccc59643d55c4fc01c52a2674f163c6
Abdullrazaq naqsho <apoodi.805@gmail.com>
2025-08-21 00:08:54 +0300

    a

--- nodex/server.js
+++ nodex/server.js
@@ -1,41 +1,37 @@
 const express = require("express");
-
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  "mongodb+srv://abdullrazaqnq13:4KFxUbAE2iThR4aJ@cluster0.3izdpk4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-
+const mongoose = require("mongoose");
 const app = express();
-let collection;
-let query;
-async function run() {
-  try {
-    // Connect the client to the server
-    await client.connect();
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-    const database = client.db("sample_mflix");
-    collection = database.collection("comments");
-    query = { name: "abdullrazaqnq13" };
-  } catch (err) {
-    // Ensures that the client will close when you finish/error
-    console.error("❌ DB Connection Error:", err);
-  }
-}
+app.use(express.json());
+const Article = require("./models/Article");
+mongoose
+  .connect(
+    "mongodb+srv://abdullrazaqnq13:4KFxUbAE2iThR4aJ@cluster0.3izdpk4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-app.get("/", async (req, res) => {
-  const comments = await collection.findOne(query);
-  res.json({ messege: "hello", data: comments });
+app.get("/articals", async (req, res) => {
+  const article = await Article.find();
+
+  res.render("../nodex/index.ejs");
 });
-run().catch(console.dir);
+
+app.post("/articals", async (req, res) => {
+  const newArticle = new Article();
+  const titleME = req.body.titleME;
+  const bodyME = req.body.bodyME;
+  const numberOfLikesME = req.body.likeME;
+  newArticle.title = titleME;
+  newArticle.body = bodyME;
+  newArticle.numberOfLikes = numberOfLikesME;
+  await newArticle.save();
+  res.render("index.html");
+  res.json(newArticle);
+  res.send("hello");
+});
 
 app.listen(3000);
